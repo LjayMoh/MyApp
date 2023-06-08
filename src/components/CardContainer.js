9c19edb81f41b56574c90e23b99780682f44ba21
LjayMoh <2194907@students.wits.ac.za>
2023-06-08 07:52:39 +0200

    update build for gh-pages

--- src/components/CardContainer.js
+++ src/components/CardContainer.js
@@ -10,28 +10,28 @@ function CardContainer() {
             <h1 className='title'> My Theory Section</h1>
             <ul className='card-items'>
               <CardObject 
-                src="images/sebastian-svenson-d2w-_1LJioQ-unsplash.jpg"
+                src="../images/sebastian-svenson-d2w-_1LJioQ-unsplash.jpg"
                 text="AI and Internet Art"
                 label="Dev Blog"
                 path="/blogs"
               />
 
               <CardObject 
-                src="images/patrick-tomasso-Oaqk7qqNh_c-unsplash.jpg"
+                src="../images/patrick-tomasso-Oaqk7qqNh_c-unsplash.jpg"
                 text="Net/Web Art"
                 label="Essay"
                 path="/blogs"
               />
 
               <CardObject 
-                src="images/zach-key-rKE6rXOl14U-unsplash.jpg"
+                src="../images/zach-key-rKE6rXOl14U-unsplash.jpg"
                 text="My Internet Art"
                 label="Dev Blog"
                 path="/blogs"
               />
 
               <CardObject 
-                src="images/florian-klauer-mk7D-4UCfmg-unsplash.jpg"
+                src="../images/florian-klauer-mk7D-4UCfmg-unsplash.jpg"
                 text="Independent Work"
                 label="Dev Blog"
                 path="/blogs"
@@ -42,28 +42,28 @@ function CardContainer() {
 
             <ul className='card-items'>
               <CardObject 
-                src="images/hal-gatewood-tZc3vjPCk-Q-unsplash.jpg"
+                src="../images/hal-gatewood-tZc3vjPCk-Q-unsplash.jpg"
                 text="Wireframes"
                 label="Design"
                 path="/design"
               />
 
               <CardObject 
-                src="images/edho-pratama-T6fDN60bMWY-unsplash.jpg"
+                src="../images/edho-pratama-T6fDN60bMWY-unsplash.jpg"
                 text="Styleguide"
                 label="Guide"
                 path="/design"
               />
 
                <CardObject 
-                src="images/estee-janssens-HAzwnsI9oEQ-unsplash.jpg"
+                src="../images/estee-janssens-HAzwnsI9oEQ-unsplash.jpg"
                 text="Rationale"
                 label="Ideas"
                 path="/design"
               />
 
               <CardObject 
-                src="images/milad-fakurian-58Z17lnVS4U-unsplash.jpg"
+                src="../images/milad-fakurian-58Z17lnVS4U-unsplash.jpg"
                 text="My UI and UX"
                 label="Design"
                 path="/design"
